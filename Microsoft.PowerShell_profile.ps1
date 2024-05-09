377d5738f78fb3efc45ceec8c4b78d7a50561728
Patin Muangjan <patinopsu@gmail.com>
2024-05-09 11:20:37 +0700

    modified credit

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,5 @@
-### PowerShell Profile Refactor
-### Version 1.03 - Refactored
+## ChrisTitusTech's PowerShell Profile
+## Modified by Patin Muangjan (patin-osu)
 
 # Initial GitHub.com connectivity check with 1 second timeout
 $canConnectToGitHub = Test-Connection github.com -Count 1 -Quiet -TimeoutSeconds 1
